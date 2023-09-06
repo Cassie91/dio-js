689fd61ae3aaac59f84a5ef9febfc7a0cc5edc3c
Alessandra da Costa Vieira <alessandracv.contato@gmail.com>
2023-09-06 12:48:36 -0300

    Class 2 - Variables

---
+++ class2/script.js
@@ -0,0 +1,127 @@
+// primitive types
+
+
+// boolean
+// var vOuF = false;
+// console.log(typeof(vOuF));
+
+// number
+// var anyNumber = 1;
+// console.log(typeof(anyNumber));
+
+// string
+// var myString = "nome";
+// console.log(typeof(myString));
+
+// var functionTest = function() {}
+// console.log(typeof(functionTest));
+
+// ------------------------------------------------------
+
+// how do I use variables
+
+
+// var variable = 'Cassie';
+// variable = 'Amy';
+// console.log(variable);
+
+// let variable2 = 'Cassie';
+// variable2 = 'Amy';
+// console.log(variable2);
+
+// const variable3 = 'Cassie';
+// const variable3 = 'Amy';
+// console.log(variable3);
+
+// ------------------------------------------------------
+
+// Scopes
+
+// var globalScope = 'global';
+// console.log(globalScope);
+
+// function localScope() {
+//   let internLocalScope = 'local';
+//   console.log(internLocalScope);
+// }
+
+// localScope();
+
+// ------------------------------------------------------
+
+// Assignment, Equality Comparison and Strict Equality Comparison.
+
+// assignment
+// var assignment = 'Cassie';
+
+// equality comparison
+// var comparison  = '0' == 0
+// console.log(comparison);
+
+// strict equality comparison
+// var strictComparison = '0' === 0
+// console.log(strictComparison);
+
+// ------------------------------------------------------
+
+// Arithmetic Operators
+
+// Addiction
+// var addiction = 1+1;
+// console.log(addiction);
+
+// Subtraction
+// var subtraction = 5-1;
+// console.log(subtraction);
+
+// Multiplication
+// var multiplication = 3*2;
+// console.log(multiplication);
+
+// Real Division > Returns the integer value.
+// var realDivision = 17/2;
+// console.log(realDivision);
+
+// Integer Division > Returns the remainder.
+// var integerDivision = 5 % 2;
+// console.log(integerDivision)
+
+//Exponentiation
+// var 	exponentiation = 5**2;
+// console.log(exponentiation);
+
+// ------------------------------------------------------
+
+// Relational operators
+
+// Greater than
+// var greaterThan = 5 > 2;
+// console.log(greaterThan);
+
+// Lower than
+// var lowerThan = 5<5;
+// console.log(lowerThan);
+
+// Greater or equal to
+// var greaterOrEqualTo = 5>=5;
+// console.log(greaterOrEqualTo);
+
+// Lower or equal to
+// var lowerOrEqualTo = 7<=7;
+// console.log(lowerOrEqualTo);
+
+// ------------------------------------------------------
+
+// Logical operators
+
+// and, &&
+// var and = (2 > 9 && 10 > 9);
+// console.log(and);
+
+//or, ||
+// var or = (2 > 9 || 10 > 9);
+// console.log(or);
+
+//not, !
+// var not = !(5>2); this means "Is not 5 greater than 2?"
+// console.log(not);
