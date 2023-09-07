bece4c0bcda3746fc1d7a2e6e6da2d45da6d40a9
Alessandra da Costa Vieira <alessandracv.contato@gmail.com>
2023-09-07 13:29:10 -0300

    Class 5 - Functions

---
+++ class5/script.js
@@ -0,0 +1,15 @@
+// Functions
+
+// function function1() {
+//   console.log("Hello, world!")
+// }
+
+// function1();
+
+//Functions with parameters.
+
+// function message(first, second) {
+//   console.log(first, second)
+// }
+
+// message('Helo, my name is', `${prompt("Hi! Please, give me a name.")}! Nice to meet you ^^`);
